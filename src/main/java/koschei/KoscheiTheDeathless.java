d5c1b770386df06ce833e11ddf97b76c8b4356fe
Vladimir <vm_77@list.ru>
2023-09-20 17:10:57 +0300

    commit

--- src/main/java/koschei/KoscheiTheDeathless.java
+++ src/main/java/koschei/KoscheiTheDeathless.java
@@ -1,5 +1,8 @@
 package koschei;
 
+import koschei.models.Deth8;
+import koschei.models.Egg6;
+import koschei.models.Needle7;
 import koschei.models.Ocean1;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -8,11 +11,24 @@ import org.springframework.stereotype.Component;
 public class KoscheiTheDeathless {
 
     private Ocean1 ocean;
-
+    private Deth8 deth8;
+    private Egg6 egg6;
+    private Needle7 needle7;
     public String getRulesByDeth() {
-        return "На свете есть океан , " + ocean.toString();
+        return "На свете есть океан , " + ocean.toString() + deth8.getDuck5() + egg6.toString() + needle7.toString();
     }
-
+    @Autowired
+    public void setNeedle7(Needle7 needle7){
+        this.needle7 = needle7;
+    }
+    @Autowired
+    public void setEgg6(Egg6 egg6){
+        this.egg6 = egg6;
+    }
+    @Autowired
+    public  void setDeth8(Deth8 deth8){
+        this.deth8 = deth8;
+    }
     @Autowired
     public void setOcean(Ocean1 ocean) {
         this.ocean = ocean;
--- src/main/java/koschei/models/Deth8.java
+++ src/main/java/koschei/models/Deth8.java
@@ -1,4 +1,16 @@
 package koschei.models;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Deth8 {
+    @Autowired
+    private Duck5 duck5;
+    @Qualifier
+    public Duck5 getDuck5() {
+       duck5.toString();
+       return duck5;
+    }
 }
